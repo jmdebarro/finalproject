367f5f4a2f06b2ac9903da09c7ee0ba2e5e56bbc
Alec Evans <alecevans20118@gmail.com>
2024-02-24 11:07:16 -0800

    implement react router

--- packages/react-frontend/src/index.js
+++ packages/react-frontend/src/index.js
@@ -1,15 +1,31 @@
 import React from "react";
 import ReactDOMClient from "react-dom/client";
 import MyApp from "./components/MyApp";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
 
 const container = document.getElementById("root");
 
 // Create a root
 const root = ReactDOMClient.createRoot(container);
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Navbar />
+        <MyApp />
+      </>
+    )
+  }
+]);
 // Initial render:
-root.render(<>
-            <Navbar/>
-            <MyApp />
-            </>);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
