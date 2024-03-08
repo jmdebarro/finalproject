e3d223b32d84504a2eb0a2cbf14a55fba511394c
Alec Evans <alecevans20118@gmail.com>
2024-03-07 18:07:23 -0800

    require auth token to post item

--- packages/react-frontend/src/components/Post.js
+++ packages/react-frontend/src/components/Post.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import './Post.css';
+import React from "react";
+import "./Post.css";
 import { useState } from "react";
 
-function Post() {
-
+function Post(props) {
   const [item, setItem] = useState({
     name: "",
     description: "",
@@ -16,12 +15,12 @@ function Post() {
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-    setItem(prevItem => ({
+    setItem((prevItem) => ({
       ...prevItem,
       [name]: value
     }));
   };
-    
+
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     if (file) {
@@ -29,9 +28,9 @@ function Post() {
       reader.onload = (e) => {
         // Get the result, and remove the prefix
         let base64String = e.target.result;
-        const base64Data = base64String.split(',')[1]; // This removes the 'data:image/jpeg;base64,' part
-  
-        setItem(prevItem => ({
+        const base64Data = base64String.split(",")[1]; // This removes the 'data:image/jpeg;base64,' part
+
+        setItem((prevItem) => ({
           ...prevItem,
           image: base64Data // Set only the pure base64 string
         }));
@@ -39,82 +38,99 @@ function Post() {
       reader.readAsDataURL(file);
     }
   };
-  
-
-  function postItem(item) {
-    const promise = fetch("https://freestuff-api.azurewebsites.net/items", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body:JSON.stringify(item),
-    });
-
-    return promise;
-  }
-
-
   function submitForm(e) {
     e.preventDefault();
     console.log(item);
-    postItem(item);
-    setItem({name: "", description: "", image: "",
-               pickUpType: "Drop Off", pickUpLocation: "", 
-               userId: "this-user", tags: "Technology"});
+    console.log("onSubmit");
+    setItem({
+      name: "",
+      description: "",
+      image: "",
+      pickUpType: "Drop Off",
+      pickUpLocation: "",
+      userId: "this-user",
+      tags: "Technology"
+    });
+    props.handleSubmit(item);
   }
 
-
-    return (
-      <div className="post-container">
-        <h1>Post an Item</h1>
-        <form onSubmit={submitForm}>
-          <div className="boxes">
-            <div className="box">
-              <h2>Photo & Description</h2>
-              <label>
-                Name:
-                <input type="text" name="name" value={item.name} onChange={handleChange} placeholder="Enter item name..." />
-              </label>
-              <label>
-                Upload Photo:
-                <input type="file" name="image" onChange={handleFileChange} accept="image/*" />
-              </label>
-              <label>
-                Description:
-                <textarea name="description" value={item.description}
-                 onChange={handleChange} placeholder="Write the item's description here..." />
-              </label>
-              <label>
-                Tags:
-                <select name="tags" value={item.tags} onChange={handleChange}>
-                  <option value="Technology">Technology</option>
-                  <option value="Kitchen">Kitchen</option>
-                  <option value="Sports">Sports</option>
-                  <option value="Furniture">Furniture</option>
-                  <option value="Art">Art</option>
-                </select>
-              </label>
-            </div>
-            <div className="box">
-              <h2>Pickup</h2>
-              <label>
-                Pickup Type:
-                <select name="pickUpType" value={item.pickUpType} onChange={handleChange}>
-                  <option value="Drop Off">Drop off</option>
-                  <option value="Meet Up">Meet up</option>
-                </select>
-              </label>
-              <label>
-                Pickup Location:
-                <textarea name="pickUpLocation" value={item.pickUpLocation}
-                   onChange={handleChange} placeholder="Write the pickup location here..." />
-              </label>
-            </div>
-          </div>
-          <button type="submit" value="Submit" >Submit</button>
-        </form>
+  return (
+    <form onSubmit={submitForm}>
+      <div className="boxes">
+        <div className="box">
+          <h2>Photo & Description</h2>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={item.name}
+              onChange={handleChange}
+              placeholder="Enter item name..."
+            />
+          </label>
+          <label>
+            Upload Photo:
+            <input
+              type="file"
+              name="image"
+              onChange={handleFileChange}
+              accept="image/*"
+            />
+          </label>
+          <label>
+            Description:
+            <textarea
+              name="description"
+              value={item.description}
+              onChange={handleChange}
+              placeholder="Write the item's description here..."
+            />
+          </label>
+          <label>
+            Tags:
+            <select
+              name="tags"
+              value={item.tags}
+              onChange={handleChange}
+            >
+              <option value="Technology">Technology</option>
+              <option value="Kitchen">Kitchen</option>
+              <option value="Sports">Sports</option>
+              <option value="Furniture">Furniture</option>
+              <option value="Art">Art</option>
+            </select>
+          </label>
+        </div>
+        <div className="box">
+          <h2>Pickup</h2>
+          <label>
+            Pickup Type:
+            <select
+              name="pickUpType"
+              value={item.pickUpType}
+              onChange={handleChange}
+            >
+              <option value="Drop Off">Drop off</option>
+              <option value="Meet Up">Meet up</option>
+            </select>
+          </label>
+          <label>
+            Pickup Location:
+            <textarea
+              name="pickUpLocation"
+              value={item.pickUpLocation}
+              onChange={handleChange}
+              placeholder="Write the pickup location here..."
+            />
+          </label>
+        </div>
       </div>
-    );
+      <button type="submit" value="Submit">
+        Submit
+      </button>
+    </form>
+  );
 }
 
 export default Post;
