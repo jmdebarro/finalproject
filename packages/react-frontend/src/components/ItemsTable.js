1b478145334507314ea4f1eb20ac4461a65e472c
Alec Evans <alecevans20118@gmail.com>
2024-03-07 18:12:21 -0800

    Revert "items from azure api"
    This reverts commit 0630f108e44975976868690a9090a7038387e9f2.

--- packages/react-frontend/src/components/ItemsTable.js
+++ packages/react-frontend/src/components/ItemsTable.js
@@ -3,18 +3,17 @@ import React, { useState, useEffect } from "react";
 import MainComponent from "./Item";
 
 function ItemsTable() {
-  const [items, setItems] = useState([]);
 
-  useEffect(() => {
-    fetchItems()
-      .then((res) => res.json())
-      .then((json) => setItems(json["items_list"]))
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+const [items, setItems] = useState([]);
 
-  return (
+useEffect(() => {
+  fetchItems()
+    .then((res) => res.json())
+    .then((json) => setItems(json["items_list"]))
+    .catch((error) => { console.log(error); });
+}, [] );
+
+return (
     <div className={"container"}>
       <MainComponent itemData={items} />
     </div>
@@ -22,10 +21,9 @@ function ItemsTable() {
 }
 
 function fetchItems() {
-  const promise = fetch(
-    "https://freestuff-api.azurewebsites.net/items"
-  );
+  const promise = fetch("http://localhost:8000/items");
   return promise;
 }
+
 
 export default ItemsTable;
