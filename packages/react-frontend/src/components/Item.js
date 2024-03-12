6c8f0885e372c5d84fa396702dcab282e5cfaf31
Jonathan DeBarro <jdebarro@calpoly.edu>
2024-03-12 12:33:33 -0700

    modified css

--- packages/react-frontend/src/components/Item.js
+++ packages/react-frontend/src/components/Item.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./table.module.css";
 import { Link } from "react-router-dom";
 
 function ItemBox(props) {
   return (
-    <Link key={props._id} className={style.item_box} to={`/item/${props._id}`} {...props}>
+    <Link
+      key={props._id}
+      className={style.item_box}
+      to={`/item/${props._id}`}
+      {...props}
+    >
       <div className={style.image}>
-          <img src={`data:image/jpeg;base64,${props.image}`} alt="Base64 Image" style={{ width: '100%', height: '100%' }} />
+        <img
+          src={`data:image/jpeg;base64,${props.image}`}
+          alt="Base64 Image"
+          style={{ width: "100%", height: "100%" }}
+        />
       </div>
       <div className={style.info}>
-        <div className={style.description}>
-          {props.description}<br/>
-          Pick Up: {props.pickUpType} <br/>
-          Location: {props.pickUpLocation}
+        <div className={style.box}>
+          Description: <br></br>{props.description}
         </div>
-        <div className={style.user}>{props.user}</div>
+        <div className={style.box}> 
+          Pick Up: <br></br> {props.pickUpType}
+        </div>
+        <div className={style.box}>
+          Location: <br></br>{props.pickUpLocation}
+        </div>
       </div>
     </Link>
   );
@@ -24,7 +36,10 @@ function MainComponent(props) {
   return (
     <div className={style.item_container}>
       {props.itemData.map((item) => (
-        <ItemBox key={item._id} {...item} />
+        <ItemBox
+          key={item._id}
+          {...item}
+        />
       ))}
     </div>
   );
