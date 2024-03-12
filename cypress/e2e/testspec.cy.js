189156d00858e5b939870db0da02b76afea68546
Jonathan DeBarro <jdebarro@calpoly.edu>
2024-03-12 13:51:24 -0700

    more ids aded to componenets

--- cypress/e2e/testspec.cy.js
+++ cypress/e2e/testspec.cy.js
@@ -6,31 +6,40 @@ describe('Navbar', () => {
   it('should render the Navbar', () => {
     cy.get('nav').should('exist') // replace 'nav' with a selector that selects your Navbar
   })
-  it('should render the Search', () => {
-    cy.get('#Search').should('exist') // replace 'nav' with a selector that selects your Navbar
-  })
-  // Add more test cases as needed
+
 })
 
-describe('Navbar', () => {
+describe('Search', () => {
   beforeEach(() => {
     cy.visit('https://delightful-island-0985f9e1e.4.azurestaticapps.net/') // replace with your app's URL
   })
 
-  it('should render the Navbar', () => {
-    cy.get('nav').should('exist') // replace 'nav' with a selector that selects your Navbar
+  it('should render the Search', () => {
+    cy.get('#Search').should('exist') // replace 'nav' with a selector that selects your Navbar
   })
+  // Add more test cases as needed
 
   // Add more test cases as needed
 })
 
-describe('Navbar', () => {
+describe('Navigate to Post', () => {
   beforeEach(() => {
     cy.visit('https://delightful-island-0985f9e1e.4.azurestaticapps.net/') // replace with your app's URL
   })
 
   it('should navigate to the post page when the Post Item button is clicked', () => {
     cy.get('button').contains('Post Item').click()
+    cy.url().should('include', '/post')
+  })
+})
+
+describe('Navigate to Settings', () => {
+  beforeEach(() => {
+    cy.visit('https://delightful-island-0985f9e1e.4.azurestaticapps.net/') // replace with your app's URL
+  })
+
+  it('should navigate to the Settings', () => {
+    cy.get('.menuButton').click()
     cy.url().should('include', '/post')
   })
 })
