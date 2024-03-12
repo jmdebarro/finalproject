42ff7a6e4e9c576ccf900448c4df775246f190f1
Alec Evans <alecevans20118@gmail.com>
2024-03-12 12:31:11 -0700

    display user contact on item page

--- packages/react-frontend/src/components/ItemPage.js
+++ packages/react-frontend/src/components/ItemPage.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { useParams,  } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import style from "./item.module.css";
-
 
 function fetchItem(id) {
   const promise = fetch(
@@ -11,14 +10,27 @@ function fetchItem(id) {
 }
 
 function ItemPage() {
-    const [item, setItem] = useState([]);
-    const { itemId } = useParams();
-    useEffect(() => {
-        fetchItem(itemId)
-          .then((res) => res.json()) 
-          .then((data) => setItem(data));
-      }, [itemId]); 
-    
+  const [item, setItem] = useState([]);
+  const [user, setUser] = useState([]);
+  const { itemId } = useParams();
+  function fetchUser(filter) {
+    const url = `https://freestuff-api.azurewebsites.net/users?userName=${filter}`;
+    const promise = fetch(url);
+    return promise;
+  }
+  useEffect(() => {
+    fetchItem(itemId)
+      .then((res) => res.json())
+      .then((data) => setItem(data))
+      .then(() => {
+        fetchUser(item.userId)
+          .then((res) => res.json())
+          .then((json) => setUser(json[0]));
+        console.log("fetched user: ");
+        console.log(user);
+        console.log(item);
+      });
+  }, [itemId, user]);
   return (
     <div key={item._id} className={style.item_box}>
       <div className={style.image}>
@@ -33,15 +45,29 @@ function ItemPage() {
           <b>Description:</b> <br></br> {item.description}
         </div>
         <div className={style.generic}>
-        <b>Pick Up:</b>  <br></br>{item.pickUpType}
+          <b>Pick Up:</b> <br></br>
+          {item.pickUpType}
         </div>
         <div className={style.generic}>
-        <b>Location:</b>  <br></br>{item.pickUpType}
+          <b>Location:</b> <br></br>
+          {item.pickUpType}
         </div>
         <div className={style.generic}>
-        <b>Filter:</b>  <br></br>{item.tags}
+          <b>Filter:</b> <br></br>
+          {item.tags}
         </div>
-        <div className={style.generic}>{item.user}</div>
+        <div className={style.generic}>
+          <b>Posted by:</b> <br></br>
+          {user ? user.userName : ""}
+        </div>
+        <div className={style.generic}>
+          <b>Phone Number:</b> <br></br>
+          {user ? user.phoneNumber : ""}
+        </div>
+        <div className={style.generic}>
+          <b>Email Address:</b> <br></br>
+          {user ? user.email : ""}
+        </div>
       </div>
     </div>
   );
