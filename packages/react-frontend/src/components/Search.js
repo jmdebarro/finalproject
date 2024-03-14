1ba5097ba8fa6fa40ddfa0f42b50ef3f831767f6
Alec Evans <alecevans20118@gmail.com>
2024-03-14 11:02:43 -0700

    Filter all button

--- packages/react-frontend/src/components/Search.js
+++ packages/react-frontend/src/components/Search.js
@@ -6,16 +6,47 @@ export default function Search() {
   const [selectedFilter, setSelectedFilter] = useState("");
   return (
     <div id="Search">
-    <div className={style.container}>
-      <div className={style.filterContainer}>
-        <h2>Filter by:</h2>
-        <button className={style.filterButton} onClick={() => setSelectedFilter("Technology")}>Technology</button>
-        <button className={style.filterButton} onClick={() => setSelectedFilter("Kitchen")}>Kitchen</button>
-        <button className={style.filterButton} onClick={() => setSelectedFilter("Sports")}>Sports</button>
-        <button className={style.filterButton} onClick={() => setSelectedFilter("Furniture")}>Furniture</button>
-        <button className={style.filterButton} onClick={() => setSelectedFilter("Art")}>Art</button>
+      <div className={style.container}>
+        <div className={style.filterContainer}>
+          <h2>Filter by:</h2>
+          <button
+            className={style.filterButton}
+            onClick={() => setSelectedFilter("")}
+          >
+            All
+          </button>
+          <button
+            className={style.filterButton}
+            onClick={() => setSelectedFilter("Technology")}
+          >
+            Technology
+          </button>
+          <button
+            className={style.filterButton}
+            onClick={() => setSelectedFilter("Kitchen")}
+          >
+            Kitchen
+          </button>
+          <button
+            className={style.filterButton}
+            onClick={() => setSelectedFilter("Sports")}
+          >
+            Sports
+          </button>
+          <button
+            className={style.filterButton}
+            onClick={() => setSelectedFilter("Furniture")}
+          >
+            Furniture
+          </button>
+          <button
+            className={style.filterButton}
+            onClick={() => setSelectedFilter("Art")}
+          >
+            Art
+          </button>
+        </div>
       </div>
-    </div>
       <ItemsTable selectedFilter={selectedFilter} />
     </div>
   );
